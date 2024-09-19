66cf7eb1a91367627ad7161d0e2826294efe99f9
Ryan Walker <you@mydomain.com>
2024-09-18 21:29:42 -0400

    init

--- cart_discount/price_discount.py
+++ cart_discount/price_discount.py
@@ -10,7 +10,14 @@ def discount(item_prices):
     Example: if this function is called with a list of [10, 4, 20] then return 4.
     """
 
-    pass  # todo replace this line with your code 
+    cheapest = item_prices[0]
+
+    if len(item_prices) >= 3:
+        for i in item_prices:
+            if i < cheapest:
+                cheapest = i
+
+        return cheapest
 
 
 if __name__ == '__main__':
--- cart_discount/test_discount.py
+++ cart_discount/test_discount.py
@@ -10,7 +10,22 @@ class TestDiscount(TestCase):
         self.assertEqual(expected_discount, discount(prices))
 
     
-    # TODO more unit tests here. Each test should test one scenario
+    def test_single_item(self):
+        princes = [1]
+        expected_discount = 1
+        self.assertEqual(expected_discount, discount(princes))
+
+
+    def test_negative_prices(self):
+        prices = [-99999999999999, -999999999995, 99999999999]
+        expected_discount = -99999999999999
+        self.assertEqual(expected_discount, discount(prices))
+
+
+    def test_prices_in_dictionary(self):
+        price = {'carrot': 2, 'donut': 4, 'banana': 1}
+        expected_discount = 1
+        self.fail('test')
 
 
 if __name__ == '__main__':
